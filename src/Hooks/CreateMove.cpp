96a8ac885f284421a508cec6c10eeee6e28724f7
Zedespook <vasszili@gmail.com>
2020-01-11 10:46:07 +0100

    Revert "Clantag changer from "BeginFrame" to "CreateMove""
    This reverts commit 18f147a62089f6383a7bc76dba63dda2d18636e8.

--- src/Hooks/CreateMove.cpp
+++ src/Hooks/CreateMove.cpp
@@ -21,7 +21,6 @@
 #include "../Hacks/esp.h"
 #include "../Hacks/tracereffect.h"
 #include "../Hacks/nofall.h"
-#include "../Hacks/clantagchanger.h"
 
 bool CreateMove::sendPacket = true;
 QAngle CreateMove::lastTickViewAngles = QAngle(0, 0, 0);
@@ -51,7 +50,6 @@ bool Hooks::CreateMove(void* thisptr, float flInputSampleTime, CUserCmd* cmd)
         EdgeJump::PrePredictionCreateMove(cmd);
 		Autoblock::CreateMove(cmd);
 		NoFall::PrePredictionCreateMove(cmd);
-		ClanTagChanger::CreateMove(cmd);
 
 		PredictionSystem::StartPrediction(cmd);
 			Aimbot::CreateMove(cmd);
